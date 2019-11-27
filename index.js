9bff1b24aee095ff3266ca9e3fa6dab22af5125d
Njiva Olaf Ranaivoson <olaf.ranaivoson@agileum.com>
2019-11-27 13:54:45 +0100

    [ update ] some comments in index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var Util = require('./lib/util');
 var FilterUtil = require('./lib/filter-util');
 var DATA = require('./constants/data');
+
+// Getting arguments from process.argv
 const args = {
     filterArgument: Util.getArgvDetailsByName(process.argv, 'filter'),
     countArgument: Util.getArgvDetailsByName(process.argv, 'count')
@@ -11,6 +13,8 @@ const filterArgs = {
     animalName: (args.filterArgument.found && args.filterArgument.value ) ? args.filterArgument.value : null,
     countFlag: args.countArgument.found
 }
-
+// Building the filtered data as from the arguments
 const result = FilterUtil.filterCountryListData(DATA.data,filterArgs.animalName,filterArgs.countFlag);
+
+// Displaying the result
 console.log(JSON.stringify(result));
