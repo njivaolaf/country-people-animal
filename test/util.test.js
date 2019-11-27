5a21bcf394821b7141e1707d763509158c4c4ee6
Njiva Olaf Ranaivoson <olaf.ranaivoson@agileum.com>
2019-11-27 11:20:32 +0100

    [ update ] getting argument details by name

--- test/util.test.js
+++ test/util.test.js
@@ -2,6 +2,8 @@ var assert = require('assert');
 
 describe('Util', function () {
     describe('#getArgvDetailsByName()', function () {
+        
+        // Note argsList is got from process.argv array
 
         it('should return { found: false ,value: null } when filter is searched and not found in arguments list', function () {
             // Mocking parameters list
@@ -25,7 +27,7 @@ describe('Util', function () {
 
         });
 
-        it('should return { found: true ,value: null } when filter is found but no value is passed', function () {
+        it('should return { found: true ,value: null } when filter is set but no value is passed', function () {
             // Mocking parameters list
             const input = {
                 argsList: [
@@ -39,6 +41,29 @@ describe('Util', function () {
             const expected = {
                 found: true,
                 value: null
+            };
+
+            var Util = require('../lib/util');
+            const result = Util.getArgvDetailsByName(input.argsList, input.argName);
+
+            assert.deepStrictEqual(result, expected);
+
+        });
+
+        it('should return { found: true ,value: "robot" } when filter is found but no value is passed', function () {
+            // Mocking parameters list
+            const input = {
+                argsList: [
+                    '/home/njivaolaf/.nvm/versions/node/v...',
+                    '/home/njivaolaf/testfolder...',
+                    '--filter=robot'
+                ],
+                argName: 'filter'
+            }
+
+            const expected = {
+                found: true,
+                value: "robot"
             };
 
             var Util = require('../lib/util');
