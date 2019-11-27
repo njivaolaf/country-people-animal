b72f8b6b5c3d01e5124770df01b2980f9e651b06
Njiva Olaf Ranaivoson <olaf.ranaivoson@agileum.com>
2019-11-27 11:42:00 +0100

    [ add ] pending tests for filter list fn ()

---
+++ test/filter-util.test.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+
+var mainData = require('../constants/data');
+var FilterUtil = require('../lib/filter-util');
+
+describe('FilterUtil', function () {
+    describe('#filterList()', function () {
+        
+        it('should return a list of countries and people with animals containing ry with intact order');
+        it('should return a list of countries and people with animals containing tula with intact order');
+        it('should return a blank list when searching for abcd');
+    });
+});
