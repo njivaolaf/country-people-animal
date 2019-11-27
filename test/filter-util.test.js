ffb7f85fa1e9d528ed76687337f4b088645b7b07
Njiva Olaf Ranaivoson <olaf.ranaivoson@agileum.com>
2019-11-27 13:11:12 +0100

    [ update ] filterCountryListData ()

--- test/filter-util.test.js
+++ test/filter-util.test.js
@@ -4,10 +4,42 @@ var mainData = require('../constants/data');
 var FilterUtil = require('../lib/filter-util');
 
 describe('FilterUtil', function () {
-    describe('#filterList()', function () {
-        
-        it('should return a list of countries and people with animals containing ry with intact order');
+    describe('#filterCountryListData()', function () {
+
+        it('should return the whole list if no filter is passed', () => {
+            const input = {
+                dataList: mainData.data,
+                filterString: null,
+                enableCount: false
+            };
+
+            const result = FilterUtil.filterCountryListData(input.dataList, input.filterString, input.enableCount);
+            const expectation = input.dataList;
+            assert.deepStrictEqual(result, expectation);
+        });
+
+        it('should return a list of countries and people with animals containing ry with intact order', () => {
+            const input = {
+                dataList: mainData.data,
+                filterString: 'ry',
+                enableCount: false
+            }
+
+            const result = FilterUtil.filterCountryListData(input.dataList, input.filterString, input.enableCount);
+
+            assert.notStrictEqual(result, null);
+            assert.strictEqual(Array.isArray(result), true);
+            assert.strictEqual(result.length, 0);
+
+            assert.deepStrictEqual(result[0].name, 'Uzuzozne');
+            assert.deepStrictEqual(result[0].people[0].name, 'Lillie Abbott');
+
+        });
+
         it('should return a list of countries and people with animals containing tula with intact order');
+
         it('should return a blank list when searching for abcd');
+
+        it('should return a list of countries and people with animals containing ry with intact order but with COUNT near the name');
     });
 });
